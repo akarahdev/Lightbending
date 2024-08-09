581d8a4ecd9a6c4586c1b8c604f8ac3f87a1c8bc
akarahdev <endisticbutitsforgit@gmail.com>
2024-08-09 13:25:33 -0400

    init repository

---
+++ src/client/java/me/endistic/lightbending/client/LightbendingClient.java
@@ -0,0 +1,10 @@
+package me.endistic.lightbending.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightbendingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/endistic/lightbending/Lightbending.java
@@ -0,0 +1,10 @@
+package me.endistic.lightbending;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Lightbending implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
